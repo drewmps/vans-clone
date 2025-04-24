3cb3de1348b48dd2f9f894c00922df99d0eb8777
drewmps <andrewmarco97@gmail.com>
2025-04-24 18:07:07 +0700

    fix: halaman products - membuat error handling di api

--- app/src/app/api/products/route.ts
+++ app/src/app/api/products/route.ts
@@ -1,7 +1,18 @@
+import CustomError from "@/db/exceptions/CustomError";
 import ProductModel from "@/db/models/ProductModel";
 
 export async function GET() {
-  const products = await ProductModel.findAll();
+  try {
+    const products = await ProductModel.findAll();
 
-  return Response.json(products);
+    return Response.json(products);
+  } catch (err) {
+    if (err instanceof CustomError) {
+      return Response.json(
+        { message: `${err.message}` },
+        { status: err.status }
+      );
+    }
+    return Response.json({ message: "Internal server error" }, { status: 500 });
+  }
 }
