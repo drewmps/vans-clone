dd110bc0953c37dbc309c9118845a1d9c15c0fc4
drewmps <andrewmarco97@gmail.com>
2025-04-24 13:01:32 +0700

    feat: halaman wishlist - menambah layout untuk authenticated

---
+++ app/src/app/(auth)/layout.tsx
@@ -0,0 +1,16 @@
+import { cookies } from "next/headers";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import React from "react";
+
+interface IProps {
+  children: React.ReactNode;
+}
+
+export default async function AuthLayout(props: IProps) {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("access_token");
+  if (!token?.value) redirect("/login");
+
+  return <>{props.children}</>;
+}
---
+++ app/src/app/(auth)/wishlist/page.tsx
@@ -0,0 +1,7 @@
+export default function WishlistPage() {
+  return (
+    <div>
+      <h1>Ini wishlist</h1>
+    </div>
+  );
+}
