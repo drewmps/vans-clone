45d5614f83948de22834f228703bd1c86152bc3d
drewmps <andrewmarco97@gmail.com>
2025-04-26 01:31:10 +0700

    refactor: memisahkan jose menjadi helper

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -1,9 +1,9 @@
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
-import * as jose from "jose";
 import CustomError from "./db/exceptions/CustomError";
 import { JWSInvalid } from "jose/errors";
+import { verifyToken } from "./db/helpers/jose";
 
 export async function middleware(request: NextRequest) {
   try {
@@ -12,13 +12,12 @@ export async function middleware(request: NextRequest) {
         const cookieStore = await cookies();
         const token = cookieStore.get("access_token");
         if (!token) throw new CustomError("Unauthorized", 401);
-        const secret = new TextEncoder().encode(process.env.SECRET_KEY_JWT);
-        const { payload } = await jose.jwtVerify<{
-          _id: string;
-          email: string;
-        }>(token.value, secret);
+
+        const payload = await verifyToken(token.value);
+
         if (!payload._id || !payload.email)
           throw new CustomError("Unauthorized", 401);
+
         const requestHeaders = new Headers(request.headers);
         requestHeaders.set("x-user-id", payload._id);
         requestHeaders.set("x-user-email", payload.email);
