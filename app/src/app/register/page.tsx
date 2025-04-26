1bcb98f2ef394b18cc207c622b6853947218feba
drewmps <andrewmarco97@gmail.com>
2025-04-26 23:33:26 +0700

    fix: halaman register - styling

--- app/src/app/register/page.tsx
+++ app/src/app/register/page.tsx
@@ -45,25 +45,40 @@ export default function RegisterPage() {
   };
   return (
     <div>
-      <h1>Register Page</h1>
-      <form onSubmit={handleRegister}>
-        <label>Name</label>
-        <input value={input.name} name="name" onChange={handleChange} />
-        <br />
-        <label>Username</label>
-        <input name="username" value={input.username} onChange={handleChange} />
-        <br />
-        <label>Email</label>
-        <input name="email" value={input.email} onChange={handleChange} />
-        <br />
-        <label>Password</label>
-        <input
-          name="password"
-          value={input.password}
-          onChange={handleChange}
-          type="password"
-        />
-        <br />
+      <form
+        onSubmit={handleRegister}
+        className="flex flex-col gap-2 mx-auto w-1/3 mt-5"
+      >
+        <h1 className="text-lg text-center">Join the Vans Family</h1>
+        <div className="border border-solid flex flex-col px-3 py-2">
+          <label>Name</label>
+          <input value={input.name} name="name" onChange={handleChange} />
+        </div>
+
+        <div className="border border-solid flex flex-col px-3 py-2">
+          <label>Username</label>
+          <input
+            name="username"
+            value={input.username}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="border border-solid flex flex-col px-3 py-2">
+          <label>Email</label>
+          <input name="email" value={input.email} onChange={handleChange} />
+        </div>
+
+        <div className="border border-solid flex flex-col px-3 py-2">
+          <label>Password</label>
+          <input
+            name="password"
+            value={input.password}
+            onChange={handleChange}
+            type="password"
+          />
+        </div>
+
         <button type="submit" className="btn btn-neutral">
           Register
         </button>
