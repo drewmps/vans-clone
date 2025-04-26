4022b32919ee519ad93d0c42abf1f0ae6dba1af4
drewmps <andrewmarco97@gmail.com>
2025-04-26 08:11:43 +0700

    feat: halaman detail product - keperluan seo

--- app/src/app/products/[slug]/page.tsx
+++ app/src/app/products/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import { IProduct } from "../page";
 import AddToWishlistButton from "@/app/components/AddToWishlistButton";
 
@@ -8,6 +9,26 @@ interface IProps {
 interface IError {
   message: string;
   status: number;
+}
+
+interface IPropsMeta {
+  params: Promise<{ slug: string }>;
+}
+export async function generateMetadata(props: IPropsMeta): Promise<Metadata> {
+  const { slug } = await props.params;
+
+  const resp = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/products/${slug}`
+  );
+  const data: IProduct = await resp.json();
+
+  return {
+    title: data.name,
+    description: data.excerpt,
+    openGraph: {
+      images: data.images,
+    },
+  };
 }
 
 export default async function ProductDetail(props: IProps) {
